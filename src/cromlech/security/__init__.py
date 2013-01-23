ab085c2e7ab46a693b5706f9017843af35fdbc95
Souheil CHELFOUH <trollfot@gmail.com>
2013-01-23 19:22:28 +0100

    exposing more API

--- src/cromlech/security/__init__.py
+++ src/cromlech/security/__init__.py
@@ -2,9 +2,13 @@
 
 from .registry import security_registry
 from .meta import secured_component, permission, security_checker
-from .interfaces import IUnauthenticatedPrincipal
+from .interfaces import IPrincipal, IUnauthenticatedPrincipal, IProtagonist
+from .interfaces import ISecuredComponent, ISecurityCheck
+from .interfaces import IAutoResolvingPermission, IPermission
 from .principal import Principal, unauthenticated_principal
 from .components import Protagonist, Interaction
 from .interaction import ContextualInteraction, ContextualProtagonist
+from .interaction import setInteraction, getInteraction
+from .interaction import restoreInteraction, deleteInteraction
 from .decorators import component_protector
 from .errors import Unauthorized, Forbidden
